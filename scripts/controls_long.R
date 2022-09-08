866bba3a88a6f879573d29c7698052ce54f14fd3
mm523 <martina.milighetti.19@ucl.ac.uk>
2022-09-08 12:04:50 +0100

    adapt

--- scripts/controls_long.R
+++ scripts/controls_long.R
@@ -1,38 +1,37 @@
-#new script which makes a control set of TCRs equal to the expanded set for alpha and betza
-#on Linux
-drive <- "/media/benny/data/"
-#on Windows
-drive<-"C:/users/Benny Chain/"
+#new script which makes a control set of TCRs equal to the expanded set for alpha and beta
 
+file_name<-load("data/output_data/exp_AB_wide3.RData")
 
-#this is the path to the master files on my computer
-input_data<-"Dropbox/Temp (1)/COVID-19/Data/"
+control_a<-list()
+control_b<-list()
 
-file<-"exp_AB_wide3.RData"
+for (chain in c("alpha", "beta")){
 
-file_name<-load(paste0(drive,input_data,file))
-chain<-"alpha"
-chain<-"beta"
-
-i_chain<-which(exp_AB_wide1$chain==chain)
-i_ctrl<-which(exp_AB_wide1$control==FALSE)
+i_chain<-which(exp_AB_wide3$chain==chain)
+i_ctrl<-which(exp_AB_wide3$control==FALSE)
 inter<-intersect(i_chain,i_ctrl)
+
+options(timeout = max(1000, getOption("timeout"))) # increasing timeout might be necessary to load the files
 
 #load controls
 
 if (chain=="alpha"){
-  load(file=paste0(drive,input_data,"all_A_long.RData"))
+  myURL<-"https://www.dropbox.com/s/a7ymcecnpomge2e/all_A_long.RData?raw=1"
+  myConnection <- url(myURL)
+  print(load(myConnection))
+  close(myConnection)
   data<-all_A_long
 rm(all_A_long)}
 
 if (chain=="beta"){
-  load(file=paste0(drive,input_data,"all_B_long.RData"))
+  myURL<-"https://www.dropbox.com/s/9kl9s4y775wam9z/all_B_long.RData?raw=1"
+  myConnection <- url(myURL)
+  print(load(myConnection))
+  close(myConnection)
   data<-all_B_long
 rm(all_B_long)}
 
-#make a control set of the sam esize as expanded non-controls
-#control_a<-list()
-control_b<-list()
+#make a control set of the same size as expanded non-controls
 
 i<-1
 for (i in 1:10){
@@ -40,10 +39,12 @@ for (i in 1:10){
   if (chain == "alpha"){control_a[[i]]<-unique(data$junction_aa)[i_ss]}
   if (chain == "beta"){control_b[[i]]<-unique(data$junction_aa)[i_ss]}
   }
-  
-  #save(control_a,file=paste0(drive,input_data,"control_alpha_1.RData"))
-  
-  save(control_b,file=paste0(drive,input_data,"control_beta_1.RData"))
+rm(data)
+
+} # close loop over chains
+
+save(control_a,file="data/output_data/control_alpha_1.RData")
+save(control_b,file="data/output_data/control_beta_1.RData")
   
 
 
