f2c6f6f1e00e54dcfa3fdf7513f64bc0ba4e89c9
marty523 <martina.milighetti.19@ucl.ac.uk>
2022-09-09 14:59:25 +0100

    clean clustering and controls

--- scripts/controls_long.R
+++ scripts/controls_long.R
@@ -10,6 +10,7 @@ for (chain in c("alpha", "beta")){
 i_chain<-which(exp_AB_wide3$chain==chain)
 i_ctrl<-which(exp_AB_wide3$control==FALSE)
 inter<-intersect(i_chain,i_ctrl)
+exp<-exp_AB_wide3[inter,]
 
 options(timeout = max(1000, getOption("timeout"))) # increasing timeout might be necessary to load the files
 
@@ -20,24 +21,46 @@ if (chain=="alpha"){
   myConnection <- url(myURL)
   print(load(myConnection))
   close(myConnection)
-  data<-all_A_long
-rm(all_A_long)}
+  all_A_long1<-all_A_long[!(duplicated(all_A_long[, c("decombinator_id", "ID")])),]
+  all_A_long1<-all_A_long1[all_A_long1$control == FALSE,]
+  data0<-all_A_long1
+  rm(all_A_long)
+  rm(all_A_long1)
+  #remove MAIT
+  i_V<-which(data0$v_call=="TRAV1-2")
+  i_J<-which(data0$j_call=="TRAJ12" | data0$j_call=="TRAJ20" | data0$j_call=="TRAJ33")
+  data1<-data0[-intersect(i_V,i_J),]
+  
+  #remove iKT
+  i_V<-which(data1$v_call=="TRAV10")
+  i_J<-which(data1$j_call=="TRAJ18")
+  data2<-data1[-(intersect(i_V,i_J)),]
+
+  nonexp_A_long<-anti_join(data2, exp, by=c("decombinator_id", "ID"))
+  data2<-nonexp_A_long
+
+rm(data0)
+rm(data1)}
 
 if (chain=="beta"){
   myURL<-"https://www.dropbox.com/s/9kl9s4y775wam9z/all_B_long.RData?raw=1"
   myConnection <- url(myURL)
   print(load(myConnection))
   close(myConnection)
-  data<-all_B_long
-rm(all_B_long)}
+  all_B_long1<-all_B_long[!(duplicated(all_B_long[, c("decombinator_id", "ID")])),]
+  all_B_long1<-all_B_long1[all_B_long1$control == FALSE,]
+  nonexp_B_long<-anti_join(all_B_long1, exp, by=c("decombinator_id", "ID"))
+  data2<-nonexp_B_long
+rm(all_B_long)
+rm(all_B_long1)}
 
 #make a control set of the same size as expanded non-controls
 
 i<-1
 for (i in 1:10){
-  i_ss<-sample(1:length(unique(data$junction_aa)),length(inter))
-  if (chain == "alpha"){control_a[[i]]<-unique(data$junction_aa)[i_ss]}
-  if (chain == "beta"){control_b[[i]]<-unique(data$junction_aa)[i_ss]}
+  i_ss<-sample(1:length(unique(data2$junction_aa)),length(inter))
+  if (chain == "alpha"){control_a[[i]]<-unique(data2$junction_aa)[i_ss]}
+  if (chain == "beta"){control_b[[i]]<-unique(data2$junction_aa)[i_ss]}
   }
 rm(data)
 
--- scripts/script_6.R
+++ scripts/script_6.R
@@ -17,22 +17,16 @@ folder_data<-"data/output_data/"  # don't save to dropbox, so if people use this
 
 # load data - using low p(gen) controls
 load(paste(folder_data, "exp_AB_wide3.RData", sep=""))
-load("data/output_data/control_alpha_2.RData") # this is a set of non-expanded sequences selected at random (Benny's script)
-load("data/output_data/control_beta_2.RData") # this is a set of non-expanded sequences selected at random (Benny's script)
+load("data/output_data/control_alpha_1.RData") # this is a set of non-expanded sequences selected at random
+load("data/output_data/control_beta_1.RData") # this is a set of non-expanded sequences selected at random 
 
 exp_a<-exp_AB_wide3[exp_AB_wide3$chain=="alpha",]
 exp_b<-exp_AB_wide3[exp_AB_wide3$chain=="beta",]
 exp_a.noC<-exp_a[exp_a$control==FALSE,] # remove PCR-
 exp_b.noC<-exp_b[exp_b$control==FALSE,] # remove PCR-
 
-a_exp<-exp_a$junction_aa
-b_exp<-exp_b$junction_aa
-
-kma_exp<-kernelMatrix(sk, a_exp, a_exp)
-kmb_exp<-kernelMatrix(sk, b_exp, b_exp)
-
-a_exp.noC<-exp_a.noC$junction_aa
-b_exp.noC<-exp_b.noC$junction_aa
+a_exp.noC<-unique(exp_a.noC$junction_aa)
+b_exp.noC<-unique(exp_b.noC$junction_aa)
 
 kma_exp.noC<-kernelMatrix(sk, a_exp.noC, a_exp.noC)
 kmb_exp.noC<-kernelMatrix(sk, b_exp.noC, b_exp.noC)
@@ -44,8 +38,8 @@ c.b<-list()
 
 for (i in 1:10){
   print(i)
-  kma.c<-kernelMatrix(sk, unlist(control_a[i]), unlist(control_a[i]))
-  kmb.c<-kernelMatrix(sk, unlist(control_b[i]), unlist(control_b[i]))
+  kma.c<-kernelMatrix(sk, unique(unlist(control_a[i])), unique(unlist(control_a[i])))
+  kmb.c<-kernelMatrix(sk, unique(unlist(control_b[i])), unique(unlist(control_b[i])))
   c.a[[i]]<-kma.c
   c.b[[i]]<-kmb.c
 }
@@ -142,18 +136,6 @@ for (j in 1:length(tha)){ # iterate over various thresholds
   avg.cluster.size.b.c[[j]]<-list()
   large.clusters.b.c[[j]]<-list()
 
-  print("1")
-
-  connect_a<-(kma_exp>=ta)
-  km_adj.a<-kma_exp
-  km_adj.a[which(connect_a)]<-1
-  km_adj.a[which(!connect_a)]<-0
-
-  connect_b<-(kmb_exp>=tb)
-  km_adj.b<-kmb_exp
-  km_adj.b[which(connect_b)]<-1
-  km_adj.b[which(!connect_b)]<-0
-
   print("2")
 #
   connect_a.noC<-(kma_exp.noC>=ta)
@@ -165,36 +147,6 @@ for (j in 1:length(tha)){ # iterate over various thresholds
   km_adj.b.noC<-kmb_exp.noC
   km_adj.b.noC[which(connect_b.noC)]<-1
   km_adj.b.noC[which(!connect_b.noC)]<-0
-#
-  print("3")
-
-  km_graph.a<-graph.adjacency(km_adj.a, mode="undirected",weighted = TRUE,add.colnames=NULL,diag=FALSE)
-  clus.a<-clusters(km_graph.a)$membership
-  clus.a.size.count<-data.frame(table(table(clus.a)))
-  perc.a<-percentage_in_cluster(clus.a)
-  perc.large.a<-percentage_in_large_cluster(clus.a)
-  avg.clus.a<-avg_clus_size(clus.a)
-  num.large.clus.a<-num_large_clusters(clus.a)
-  clus.size.count.a<-c(clus.size.count.a, clus.a.size.count)
-  perc.clustering.a<-c(perc.clustering.a, perc.a)
-  perc.clustering.large.a<-c(perc.clustering.large.a, perc.large.a)
-  avg.cluster.size.a<-c(avg.cluster.size.a, avg.clus.a)
-  large.clusters.a<-c(large.clusters.a, num.large.clus.a)
-
-  print("4")
-
-  km_graph.b<-graph.adjacency(km_adj.b, mode="undirected",weighted = TRUE,add.colnames=NULL,diag=FALSE)
-  clus.b<-clusters(km_graph.b)$membership
-  clus.b.size.count<-data.frame(table(table(clus.b)))
-  perc.b<-percentage_in_cluster(clus.b)
-  perc.large.b<-percentage_in_large_cluster(clus.b)
-  avg.clus.b<-avg_clus_size(clus.b)
-  num.large.clus.b<-num_large_clusters(clus.b)
-  clus.size.count.b<-c(clus.size.count.b, clus.b.size.count)
-  perc.clustering.b<-c(perc.clustering.b, perc.b)
-  perc.clustering.large.b<-c(perc.clustering.large.b, perc.large.b)
-  avg.cluster.size.b<-c(avg.cluster.size.b, avg.clus.b)
-  large.clusters.b<-c(large.clusters.b, num.large.clus.b)
 
   print("5")
 #
@@ -339,27 +291,7 @@ mylevels<-c(">100", "11-100", "5-10", "3-4", "doublet", "singlet")
 clus.size.count.a.all$c.size.cat<-factor(clus.size.count.a.all$c.size.cat, levels=mylevels)
 cols<-c("chocolate4", "lightsalmon", "moccasin", "lightskyblue", "lightsteelblue1", "lightsteelblue")
 names(cols)<-mylevels
-# ggplot(clus.size.count.a.all, aes(x=type, fill=c.size.cat, y=prop, )) +
-#   geom_bar(stat="identity", color="white") +
-#   facet_grid(.~thresh) +
-#   scale_fill_manual(values=cols) +
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank())+
-#   labs(fill="cluster size", y="proportion") +
-#   ggtitle("alpha")
-
-# ggplot(clus.size.count.a.all, aes(x=type, fill=c.size.cat, y=tot.count, )) +
-#   geom_bar(stat="identity", color="white") +
-#   facet_grid(.~thresh) +
-#   scale_fill_manual(values=cols) +
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank()) +
-#   labs(fill="cluster size", y="count") +
-#   ggtitle("alpha")
-# 
-# 
+ 
 large.clus.b.c<-data.frame(do.call(rbind, large.clusters.b.c))
 large.clus.b<-data.frame(do.call(rbind, large.clusters.b.noC))
 large.clus.b$thres<-thb
@@ -387,28 +319,6 @@ clus.size.count.b.all$c.size.cat<-ifelse(clus.size.count.b.all$c.size == 1, "sin
                                                        ifelse(clus.size.count.b.all$c.size < 11, "5-10",
                                                               ifelse(clus.size.count.b.all$c.size < 101, "11-100", ">100")))))
 clus.size.count.b.all$c.size.cat<-factor(clus.size.count.b.all$c.size.cat, levels=mylevels)
-# 
-# ggplot(clus.size.count.b.all, aes(x=type, fill=c.size.cat, y=prop, )) +
-#   geom_bar(stat="identity", color="white") +
-#   facet_grid(.~thresh) +
-#   scale_fill_manual(values=cols) +
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank())+
-#   labs(fill="cluster size", y="proportion") +
-#   ggtitle("beta")
-# 
-# ggplot(clus.size.count.b.all, aes(x=type, fill=c.size.cat, y=tot.count, )) +
-#   geom_bar(stat="identity", color="white") + 
-#   facet_grid(.~thresh) +
-#   scale_fill_manual(values=cols) +
-#   theme(axis.title.x=element_blank(),
-#         axis.text.x=element_blank(),
-#         axis.ticks.x=element_blank()) +
-#   labs(fill="cluster size", y="count") +
-#   ggtitle("beta")
-# 
-# 
 
 ## more functions to faciliate plotting
 
@@ -462,53 +372,6 @@ plot_results<-function(x, shape=16){
   return(p)
 }
 
-plot_cluster_size_lineplot<-function(counts){
-  # number of clusters of each size
-  mycolors<-c("grey", "grey","grey","grey","grey","grey",
-              "grey","grey","grey","grey", "red")
-  counts.grouped<-counts %>% group_by(c.size, type) %>% 
-    summarise(counts = sum(count))
-  
-  df1<-data.frame(counts.grouped)
-  
-  df2 <- data.frame(df1[rep(seq_len(dim(df1)[1]),  with(df1, ifelse(counts > 0 & !is.na(counts), counts, 1))
-  ), , drop = FALSE], row.names=NULL)
-  df3<-data.frame(table(df2[,c("type", "c.size")]))
-  
-  p<- ggplot(df3, aes(x=as.numeric(as.character(c.size)), y=Freq, col = type, group = type)) +
-    geom_point(size = 2, alpha = 0.5) + geom_line() +
-    scale_color_manual(values=mycolors, guide = "none") +
-    labs(x = "cluster size", y = "number of clusters", col = "") +
-    scale_y_continuous(breaks = scales::pretty_breaks(n = 15), trans="sqrt") +
-    geom_hline(yintercept = 0, col = "lightgrey", lty = "dashed") +
-    theme_bw()
-  return(p)
-}
-
-plot_cluster_size_barplot<-function(counts){
-  # number of clusters of each size (barplot)
-  mycolors<-c("grey", "grey","grey","grey","grey","grey",
-              "grey","grey","grey","grey", "red")
-  
-  counts.grouped<-counts %>% group_by(c.size.cat, type) %>% 
-    summarise(counts = sum(count))
-  
-  df1<-data.frame(counts.grouped)
-  
-  df2 <- data.frame(df1[rep(seq_len(dim(df1)[1]),  with(df1, ifelse(counts > 0 & !is.na(counts), counts, 1))
-  ), , drop = FALSE], row.names=NULL)
-  df3<-data.frame(table(df2[,c("type", "c.size.cat")]))
-  
-  p<- ggplot(df3, aes(x=c.size.cat, y=Freq, fill = type)) +
-    geom_bar(stat="identity", position = position_dodge(), col = "black") +
-    scale_fill_manual(values=mycolors, drop = FALSE, guide = "none") +
-    labs(x = "cluster size", y = "number of clusters", col = "") +
-    scale_y_continuous(breaks = scales::pretty_breaks(n = 15), trans="sqrt") +
-    theme_bw() +
-    scale_x_discrete(drop=FALSE)
-  return(p)
-}
-
 ## plot all
 
 perc.a<-prepare_results(perc.clus.a.c, perc.clus.a)
@@ -527,23 +390,6 @@ svg("output_figures/FigS12_alpha.svg")
 print(pa)
 dev.off()
 
-# clus.size.count.a.mythresh<-clus.size.count.a.all[clus.size.count.a.all$thresh == 0.76,]
-
-# pa_lineplot<-plot_cluster_size_lineplot(clus.size.count.a.mythresh)
-# pa_lineplot + ggtitle("CDR3a, th = 0.76, clustering profile")
-# 
-# pa_barplot<-plot_cluster_size_barplot(clus.size.count.a.mythresh)
-# pa_barplot + ggtitle("CDR3a, th = 0.76, clustering profile")
-
-# 
-# perc.large.a<-prepare_results(perc.clus.large.a.c, perc.clus.large.a)
-# p.perc.large.a<-plot_results(perc.large.a)
-# p.perc.large.a +
-#   geom_vline(xintercept = 0.76, lty="dashed") +
-#   xlab("threshold set for clustering") +
-#   ylab("Proportion clustering > 4") +
-#   ggtitle("Proportion of CDR3s in large clusters, alpha")
-
 perc.b<-prepare_results(perc.clus.b.c, perc.clus.b)
 p.perc.b<-plot_results(perc.b)
 pb<-p.perc.b +
@@ -559,51 +405,3 @@ pb<-p.perc.b +
 svg("output_figures/FigS12_beta.svg")
 print(pb)
 dev.off()
-
-# clus.size.count.b.mythresh<-clus.size.count.b.all[clus.size.count.b.all$thresh == 0.72,]
-# 
-# pb_lineplot<-plot_cluster_size_lineplot(clus.size.count.b.mythresh)
-# pb_lineplot + ggtitle("CDR3b, th = 0.72, clustering profile")
-# 
-# pb_barplot<-plot_cluster_size_barplot(clus.size.count.b.mythresh)
-# pb_barplot + ggtitle("CDR3b, th = 0.72, clustering profile")
-
-# perc.large.b<-prepare_results(perc.clus.large.b.c, perc.clus.large.b)
-# p.perc.large.b<-plot_results(perc.large.b)
-# p.perc.large.b +
-#   geom_vline(xintercept = 0.72, lty="dashed") +
-#   xlab("threshold set for clustering") +
-#   ylab("Proportion clustering > 4") +
-#   ggtitle("Proportion of CDR3s in large clusters, beta")
-
-# large.a<-prepare_results(large.clus.a.c, large.clus.a)
-# p.large.a<-plot_results(large.a)
-# p.large.a +
-#   geom_vline(xintercept = 0.76, lty="dashed") +
-#   xlab("threshold set for clustering") +
-#   ylab("Clusters > 4") +
-#   ggtitle("Number of large clusters, alpha")
-# 
-# large.b<-prepare_results(large.clus.b.c, large.clus.b)
-# p.large.b<-plot_results(large.b)
-# p.large.b +
-#   geom_vline(xintercept = 0.72, lty="dashed") +
-#   xlab("threshold set for clustering") +
-#   ylab("Clusters > 4") +
-#   ggtitle("Number of large clusters, beta")
-
-# size.a<-prepare_results(avg.clus.a.c, avg.clus.a)
-# p.size.a<-plot_results(size.a)
-# p.size.a +
-#   geom_vline(xintercept = 0.76, lty="dashed") +
-#   xlab("threshold set for clustering") +
-#   ylab("Average cluster size") +
-#   ggtitle("Average size of clusters, alpha")
-# 
-# size.b<-prepare_results(avg.clus.b.c, avg.clus.b)
-# p.size.b<-plot_results(size.b)
-# p.size.b +
-#   geom_vline(xintercept = 0.72, lty="dashed") +
-#   xlab("threshold set for clustering") +
-#   ylab("Average cluster size") +
-#   ggtitle("Average size of clusters, beta")
